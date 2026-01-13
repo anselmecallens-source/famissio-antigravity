5161333f247bb6cb92bf075b32b7190743bdfdfa
anselmecallens-source <anselmecallens@gmail.com>
2026-01-13 21:58:32 +0100

    Cleanup and force update

--- famissio/src/pages/Home.jsx
+++ famissio/src/pages/Home.jsx
@@ -1,5 +1,4 @@
-﻿// Trigger Vercel Deploy 1
-import React, { useState, useEffect } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import HeroCarousel from '../components/HeroCarousel';
 
